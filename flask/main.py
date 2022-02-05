b7ffe6a8790cb2e6446e1c6befc407875339514c
himanshu8160 <himanshusharma445653@gmail.com>
2022-02-05 08:33:38 +0530

    changes required

--- flask/main.py
+++ flask/main.py
@@ -65,16 +65,17 @@ def checkVideo():
                 val2= str(p[0][1]);
                 val3= str(p[0][2]); 
                 if 'e' in val1:
-                    p[0][0]=math.log(round(p[0][0], 2))
+                    p[0][0]=math.log(float(val1[:4]))
+                    print(p[0][0])
                 if 'e' in val2:
-                    p[0][1]=math.log(round(p[0][1], 2))
+                    p[0][1]=math.log(float(val2[:4]))
+                    print(p[0][1])
                 if 'e' in val3:
-                    p[0][2]=math.log(round(p[0][2], 2))
-                
-                if 'e' not in val1:
-                    print("not")
+                    p[0][2]=math.log(float(val3[:4]))
+                    print(p[0][2])
+              
                 result.append(p)
-                print(p[0][0]);
+             
                 print(c)
             else:
                 break
--- server/routes/videoRouter.js
+++ server/routes/videoRouter.js
@@ -45,7 +45,7 @@ const storage = new GridFsStorage({
     });
   },
 });
-const upload = multer({ storage });
+const upload = multer({ storage }).single("file");;
 
 const videoRouter = Router();
 const videoStorage = multer.diskStorage({
@@ -75,35 +75,50 @@ const videoUpload = multer({
 videoRouter
   .route("/videoUpload")
   .post(videoUpload.single("file"), function (req, res) {
-    console.log(req.body);
+    let body =req.body;
     console.log(req.body.category);
     console.log(req.file.filename);
 
     const url = "http://127.0.0.1:5000/checkVideo?name=" + req.file.filename;
 
-    //  let video = new Video({
-    //   title: req.body.title,
-    //   description: req.body.description,
-    //   categoryId: req.body.category,
-    //   status: req.body.category == 1 ? "private" : "public",
-    //   file: req.file.id
-    //  });
+   
 
     request(url, function (error, response, body) {
       console.error("error:", error); // Print the error
       console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
       console.log("body:", body); // Print the data received
       console.log("in rew");
+
+      
+        upload(req,res,function(err) {
+          if(err) {
+              console.log(err);
+          }
+          
+          let video = new Video({
+            title: body.title,
+            description: body.description,
+            categoryId: body.category,
+            status: body.type == 1 ? "private" : "public",
+            file: req.file.id
+          });
+          video.save()
+          .then(result => {
+          res.status(200).json({ 'video': 'video Added Successfully' });
+          })
+          .catch(err => {
+            console.log(err);
+          res.status(400).send(err);
+          });
+      });
+        
+        
+      });
+
+     
     });
 
-    //  video.save()
-    //  .then(result => {
-    //  res.status(200).json({ 'video': 'video Added Successfully' });
-    //  })
-    //  .catch(err => {
-    //    console.log(err);
-    //  res.status(400).send(err);
-    //  });
-  });
+   
+  
 
 export default videoRouter;
