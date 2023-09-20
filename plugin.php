f3b480c83e48cb3537f9dca92866963e2265e57b
Niraj Gautam <nirajgautam196@gmail.com>
2023-09-20 18:53:06 +0530

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,20 @@
+/*
+Plugin Name: Name of the plugin.
+Plugin URI: The home page of the plugin.
+Description: Plugin description.
+Author: Plugin author's name.
+Author URI: Link to the author's website.
+Version: Plugin version.
+Text Domain: Optional. Unique identifier, should be same as the one used in
+     load_plugin_textdomain().
+Domain Path: Optional. Only useful if the translations are located in a
+     folder above the plugin's base path. For example, if .mo files are
+     located in the locale folder then Domain Path will be "/locale/" and
+     must have the first slash. Defaults to the base folder the plugin is
+     located in.
+Network: Optional. Specify "Network: true" to require that a plugin is activated
+     across all sites in an installation. This will prevent a plugin from being
+     activated on a single site when Multisite is enabled.
+Requires at least: Optional. Specify the minimum required WordPress version.
+Requires PHP: Optional. Specify the minimum required PHP version.
+* /
